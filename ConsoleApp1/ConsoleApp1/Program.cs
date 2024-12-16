00ad2ed0e90a9856f6ec31eadbcc01d53f1a41c2
Moaz Abd El-Fattah <moazabdelfattah9@gmail.com>
2024-12-16 16:24:58 +0200

    C#_Assignment02

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region Question01
+            //Q1
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("You entered: " + input);
+            #endregion
+            
+            #region Question02
+            //string inputWithNonNumeric = "123ABC";
+            //int convertedNumber = Convert.ToInt32(inputWithNonNumeric);
+            //Console.WriteLine("Converted number: " + convertedNumber);
+            #endregion
+            
+            #region Question03
+            //double num1 = 5.5;
+            //double num2 = 4.5;
+            //double result = num1 + num2;
+            //Console.WriteLine("Result of addition: " + result);
+            #endregion
+            
+            #region Question04
+
+            //string str = "Hello, world!";
+            //string substring = str.Substring(7, 5);
+            //Console.WriteLine("Extracted substring: " + substring);
+            #endregion
+            
+            #region Question05
+            //int a = 10;
+            //int b = a; 
+            //a = 20; 
+            //Console.WriteLine( a); 
+            //Console.WriteLine( b);
+            #endregion
+            
+            #region Question06
+            //int[] numbers1 = { 7, 8, 9 };
+            //int[] numbers2 = numbers1;
+            //numbers2[2] = 99;
+            //Console.WriteLine(numbers1[2]); 
+            //Console.WriteLine(numbers2[2]);
+            #endregion
+            
+            #region Question07
+            //string str1 = "Hello";
+            //string str2 = "World";
+            //string result1 = str1 + " " + str2; 
+            //Console.WriteLine(result1); 
+            #endregion
+        }
+    }
+}
+
+
